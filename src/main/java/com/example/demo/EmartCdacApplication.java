1225c2585d02c1382dcb7674b1f742e97d517ea7
Kodilkarshreyanshu <kodilkarshreyanshu@gmail.com>
2026-01-20 09:48:45 +0530

    initial commit

---
+++ src/main/java/com/example/demo/EmartCdacApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmartCdacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmartCdacApplication.class, args);
+	}
+
+}
