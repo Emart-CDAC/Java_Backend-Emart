333afca37a0deaf7780e53141b4e0ccadd5c9d28
Raashii <sabalerashi@gmail.com>
2026-01-20 12:10:02 +0530

    rashi cart commit

---
+++ src/main/java/com/example/model/Cart.java
@@ -0,0 +1,48 @@
+package com.example.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Cart")
+public class Cart {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="Cart_Id")
+	private int Cart_Id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "User_Id")
+	private Customer customer;
+	
+	@Column(name="Total_Amount", precision=10,scale=2)
+	private BigDecimal Total_Amount;
+	
+	
+	public int getCartId() {
+		return Cart_Id;
+	}
+	
+	public void setCartId(int cartId) {
+		this.Cart_Id=cartId;
+	}
+	
+	
+	public BigDecimal getTotalAmount() {
+		return Total_Amount;
+	}
+	
+	public void setTotalAmount(BigDecimal totalAmount) {
+		this.Total_Amount=totalAmount;
+	}
+}
