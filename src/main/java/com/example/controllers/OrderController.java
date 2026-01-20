1523508698c74b4844968393dcf6d15dc4e8342f
Your Name <you@example.com>
2026-01-21 00:30:32 +0530

    swayam_commit

--- src/main/java/com/example/controllers/OrderController.java
+++ src/main/java/com/example/controllers/OrderController.java
@@ -1,10 +1,7 @@
 package com.example.controllers;
 
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.Orders;
-import com.example.model.Product;
-import com.example.services.OrderService;
-
-import jakarta.servlet.http.HttpServletResponse;
+import com.example.services.OrdersService;
 
 @RestController
 @RequestMapping("/order")
@@ -27,19 +21,19 @@ public class OrderController
 {
 	
 	@Autowired
-	private final OrderService orderService;
+	private final OrdersService orderService=null;
 	
 	@PostMapping("/placeOrder")
 	public ResponseEntity<Orders> placeOrder(@RequestBody Orders orders)
 	{
-		Orders order = orderService.place(orders);
+		Orders order = orderService.addOrder(orders);
 		return ResponseEntity.ok(order);
 	}
 	
 	@GetMapping("/history")
-	public ResponseEntity<List<Orders>> getOrderHistory(@PathVariable int user_id)
+	public ResponseEntity<Optional<List<Orders>>> getOrderHistory(@PathVariable int user_id)
 	{
-		List<Orders> orders = orderService.getById(user_id);
+		Optional<List<Orders>> orders = orderService.getOrderById(user_id);
 		return ResponseEntity.ok(orders);
 	}
 	
--- src/main/java/com/example/services/InvoiceService.java
+++ src/main/java/com/example/services/InvoiceService.java
@@ -1,10 +1,13 @@
 package com.example.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.example.model.Invoice;
 
 public interface InvoiceService 
 {
 	Invoice addInvoice(int order_id);
-	Invoice findById(int invoice_id);
-	
+	Optional<Invoice> findById(int invoice_id);
+	List<Invoice> findAll();
 }
