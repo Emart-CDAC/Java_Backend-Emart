66041ce7ea64b4b3cebc785ddb4e7be44f5caf63
Tk <kaletanmayee1@gmail.com>
2026-01-22 13:55:26 +0530

    jwt changes

---
+++ src/main/java/com/example/security/JwtUtil.java
@@ -0,0 +1,57 @@
+package com.example.security;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+	@Value("${jwt.secret}")
+	private String secret;
+
+	private Key getSigningKey() {
+		return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+	}
+
+	public String extractEmail(String token) {
+		return getClaims(token).getSubject();
+	}
+
+	public String extractRole(String token) {
+	    return getClaims(token).get("role", String.class);
+	}
+
+	public Date extractExpiration(String token) {
+		return getClaims(token).getExpiration();
+	}
+
+	public boolean isValid(String token) {
+		try {
+			getClaims(token);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	private Claims getClaims(String token) {
+		return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
+	}
+
+	public boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+
+	public String generateToken(String email, String role) {
+		return Jwts.builder().setSubject(email).claim("role", role).setIssuer("EmartAuthService")
+				.setIssuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis() + 1 * 60 * 60 * 1000))
+				.signWith(getSigningKey()).compact();
+	}
+
+}
